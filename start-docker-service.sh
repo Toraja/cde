44b005c91c50ee48f8c6a2eaa533dda552cb47f8
Toraja <toraja.blend.french@gmail.com>
2021-10-01 12:42:58 +0900

    supports starting up docker desktop

--- start-docker-service.sh
+++ start-docker-service.sh
@@ -6,6 +6,10 @@ is_wsl() {
 	grep -q -i microsoft /proc/version && return 0 || return 1
 }
 
+is_docker_desktop() {
+	test -L ~/.docker/contexts && return 0 || return 1
+}
+
 get_dist() {
 	is_wsl && echo ${dist_wsl} && return
 }
@@ -28,6 +32,17 @@ main() {
 
 	case $(get_dist) in
 		${dist_wsl})
+			if is_docker_desktop; then
+				if [[ -f /mnt/c/Program\ Files/Docker/Docker/Docker\ Desktop.exe ]]; then
+					  /mnt/c/Program\ Files/Docker/Docker/Docker\ Desktop.exe &
+					  echo ${cyan}Docker Desktop has been dispatched.${nc}
+					  return 0
+				else
+					echo 'Docker Desktop excutable was not found. Start it manually.'
+					exit 1
+				fi
+			fi
+
 			start_docker_init_script
 			;;
 		*)
