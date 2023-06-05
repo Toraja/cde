56a16fab11911a7f5d94881ad49ba99010598dc5
Toraja <Toraja@users.noreply.github.com>
2023-06-05 21:37:37 +0900

    root: install lua using catalog

--- root/ctx/helpers/install-asdf-plugins.sh
+++ root/ctx/helpers/install-asdf-plugins.sh
@@ -10,8 +10,6 @@ source ~/.asdf/asdf.sh
 asdf-global-installer.sh \
     direnv \
     neovim \
-    lua \
-    stylua \
     nodejs \
     grpcurl \
     ghq \
@@ -33,8 +31,6 @@ asdf-global-installer.sh \
 asdf direnv setup --shell fish --version latest
 just --completions fish > ~/.config/fish/completions/just.fish
 helm plugin install https://github.com/databus23/helm-diff
-luarocks completion fish > ~/.config/fish/completions/luarocks.fish
-luarocks install busted
 npm install --global yaml-language-server
 kubectl completion fish > ~/.config/fish/completions/kubectl.fish
 kind completion fish > ~/.config/fish/completions/kind.fish
