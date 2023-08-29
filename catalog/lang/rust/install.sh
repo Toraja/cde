f9d6823b5cbf92ceac7fd4abc2bb40e7ce058896
Toraja <Toraja@users.noreply.github.com>
2023-08-29 10:14:37 +0900

    catalog: install rust-analyzer via rustup as it seems to be required

--- catalog/lang/rust/install.sh
+++ catalog/lang/rust/install.sh
@@ -11,6 +11,6 @@ source ${HOME}/.asdf/asdf.sh
 # protoc is required by rust-analyzer
 asdf-global-installer.sh rust rust-analyzer protoc sccache
 rustup completions fish > ~/.config/fish/completions/rustup.fish
-rustup component add rust-src rust-analysis rustfmt clippy
+rustup component add rust-src rust-analysis rustfmt clippy rust-analyzer
 # required by nvim-neotest
 curl --location --silent --show-error --fail https://get.nexte.st/latest/linux | tar -zxf - --directory $(dirname $(asdf which cargo))
