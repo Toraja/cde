7448635acac482c6a2b28f9e0a47ba962ddd9ae4
Toraja <Toraja@users.noreply.github.com>
2024-01-10 13:57:24 +0900

    catalog/lua: install lua-language-server via asdf

--- catalog/lang/lua/install.sh
+++ catalog/lang/lua/install.sh
@@ -7,8 +7,7 @@ sudo apt-get install --no-install-recommends --yes \
     build-essential
 
 source ${HOME}/.asdf/asdf.sh
-asdf-global-installer.sh lua stylua
-github-latest-release-installer.sh -t LuaLS lua-language-server 'lua-language-server-.*-linux-x64.tar.gz' ~/.local/opt/lua-language-server/
+asdf-global-installer.sh lua stylua lua-language-server
 
 luarocks completion fish > ~/.config/fish/completions/luarocks.fish
 luarocks install busted
