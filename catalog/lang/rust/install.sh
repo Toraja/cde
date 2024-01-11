cd295ff97a426b5fe98768040c9355214eca14cf
Toraja <Toraja@users.noreply.github.com>
2024-01-11 12:11:01 +0900

    catalog/rust: enable specifying rust version, remove duplicate rust-analyzer installation and remove unnecessary rustup component add

--- catalog/lang/rust/install.sh
+++ catalog/lang/rust/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 
+rust_version=${1:-latest}
+
 sudo apt-get update
 sudo apt-get upgrade --no-install-recommends --yes
 # pkg-config: required by rust-openssl crate which reqwest crate depends on
@@ -11,8 +13,8 @@ sudo apt-get install --no-install-recommends --yes \
 
 source ${HOME}/.asdf/asdf.sh
 # protoc is required by rust-analyzer
-asdf-global-installer.sh rust rust-analyzer protoc sccache
+asdf-global-installer.sh rust:${rust_version} protoc sccache
 rustup completions fish > ~/.config/fish/completions/rustup.fish
-rustup component add rust-src rust-analysis rustfmt clippy rust-analyzer
+rustup component add rust-analyzer
 # required by nvim-neotest
 curl --location --silent --show-error --fail https://get.nexte.st/latest/linux | tar -zxf - --directory $(dirname $(asdf which cargo))
