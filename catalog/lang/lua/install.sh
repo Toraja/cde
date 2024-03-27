cfffa2ece7513a3f01f079cf1e71c74ec206fcb9
Toraja <Toraja@users.noreply.github.com>
2024-03-27 17:53:28 +0900

    catalog/lua: add luajit and pin lua version to 5.1.5

--- catalog/lang/lua/install.sh
+++ catalog/lang/lua/install.sh
@@ -7,7 +7,8 @@ sudo apt-get install --no-install-recommends --yes \
     build-essential
 
 source ${HOME}/.asdf/asdf.sh
-asdf-global-installer.sh lua stylua lua-language-server
+# neorg (luarocks.nvim) requires lua version to be 5.1
+asdf-global-installer.sh lua:5.1.5 luajit stylua lua-language-server
 
 luarocks completion fish > ~/.config/fish/completions/luarocks.fish
 luarocks install busted
