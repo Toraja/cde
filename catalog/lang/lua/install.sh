3cfe0533f35ed2378ebc0a518d7fe9254bd50e88
Toraja <Toraja@users.noreply.github.com>
2024-04-01 16:51:18 +0900

    catalog/lua: fix intallation by adding readline

--- catalog/lang/lua/install.sh
+++ catalog/lang/lua/install.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 sudo apt-get update
 sudo apt-get upgrade --no-install-recommends --yes
 sudo apt-get install --no-install-recommends --yes \
-    build-essential
+    build-essential libreadline-dev
 
 source ${HOME}/.asdf/asdf.sh
 # neorg (luarocks.nvim) requires lua version to be 5.1
+# If somehow luarocks.nvim supports lua 5.4, add ASDF_LUA_LINUX_READLINE=1
+# See https://github.com/Stratus3D/asdf-lua?tab=readme-ov-file#linux-readline for more detail
 asdf-global-installer.sh lua:5.1.5 luajit stylua lua-language-server
 
 luarocks completion fish > ~/.config/fish/completions/luarocks.fish
