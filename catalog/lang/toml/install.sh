d3be25209abc3f63dc7a52e152d3b8dc39620d19
Toraja <Toraja@users.noreply.github.com>
2024-04-01 16:50:14 +0900

    catalog/toml: pin the version to 0.8.1 as the current latest version (0.9.0) does not have assets

--- catalog/lang/toml/install.sh
+++ catalog/lang/toml/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-github-latest-release-installer.sh tamasfe taplo 'taplo-linux-x86_64.gz' /tmp/taplo.gz
+# github-latest-release-installer.sh tamasfe taplo 'taplo-linux-x86_64.gz' /tmp/taplo.gz
+# NOTE: taple 0.9.0 does not have assets and hence fails to install.
+curl -o /tmp/taplo.gz -fsSL https://github.com/tamasfe/taplo/releases/download/0.8.1/taplo-linux-x86_64.gz
 gunzip /tmp/taplo.gz
 chmod 755 /tmp/taplo
-mv taplo ~/.local/bin/
+mv /tmp/taplo ~/.local/bin/
