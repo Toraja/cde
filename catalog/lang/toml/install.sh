f7aa4d5f586fe8f92af1876dbcc96544b5a2c414
Toraja <Toraja@users.noreply.github.com>
2024-07-22 19:25:55 +0900

    catalog/toml: download taplo via github-latest-release-installer as the assets are now listed

--- catalog/lang/toml/install.sh
+++ catalog/lang/toml/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-# github-latest-release-installer.sh tamasfe taplo 'taplo-linux-x86_64.gz' /tmp/taplo.gz
-# NOTE: taple 0.9.0 does not have assets and hence fails to install.
-curl -o /tmp/taplo.gz -fsSL https://github.com/tamasfe/taplo/releases/download/0.8.1/taplo-linux-x86_64.gz
+github-latest-release-installer.sh tamasfe taplo 'taplo-full-linux-x86_64.gz' /tmp/taplo.gz
 gunzip /tmp/taplo.gz
 chmod 755 /tmp/taplo
 mv /tmp/taplo ~/.local/bin/
