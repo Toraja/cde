79a61068aeb30c880c68de32c7b2898f759e5d1d
Toraja <Toraja@users.noreply.github.com>
2024-09-03 18:33:42 +0900

    catalog/lua: change the way to install luajit

--- catalog/lang/lua/install.sh
+++ catalog/lang/lua/install.sh
@@ -9,7 +9,13 @@ source ${HOME}/.asdf/asdf.sh
 # neorg (luarocks.nvim) requires lua version to be 5.1
 # If somehow luarocks.nvim supports lua 5.4, add ASDF_LUA_LINUX_READLINE=1
 # See https://github.com/Stratus3D/asdf-lua?tab=readme-ov-file#linux-readline for more detail
-asdf-global-installer.sh lua:5.1.5 luajit stylua lua-language-server
+asdf-global-installer.sh lua:5.1.5 stylua lua-language-server
+# luajit is avaialbe as asdf plugin but it now fails to install, so build manually instead
+tmp_luajit=/tmp/LuaJIT
+git clone https://github.com/LuaJIT/LuaJIT.git $tmp_luajit
+cd $tmp_luajit
+make && sudo make install
+rm -rdf $tmp_luajit
 
 luarocks completion fish > ~/.config/fish/completions/luarocks.fish
 luarocks install busted
