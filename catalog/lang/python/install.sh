df42547f4fb084c879c78af0973bb0d4772b50c4
Toraja <Toraja@users.noreply.github.com>
2024-09-26 11:51:53 +0900

    catalog/python: add liblzma

--- catalog/lang/python/install.sh
+++ catalog/lang/python/install.sh
@@ -2,13 +2,15 @@
 set -eo pipefail
 
 sudo apt-get update
+# liblzma-dev is required by cohere package
 sudo apt-get install --no-install-recommends --yes \
     libedit-dev \
     zlib1g \
     zlib1g-dev \
     libssl-dev \
     libbz2-dev \
-    libsqlite3-dev
+    libsqlite3-dev \
+    liblzma-dev
 
 source ${HOME}/.asdf/asdf.sh
 asdf-global-installer.sh python
