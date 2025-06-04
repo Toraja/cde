dcb0de7b5419f37f9c2ef1c5909361b039951348
Toraja <Toraja@users.noreply.github.com>
2025-06-04 16:46:15 +0900

    catalog/python install some packages using mise

--- catalog/lang/python/install.sh
+++ catalog/lang/python/install.sh
@@ -4,17 +4,23 @@ set -eo pipefail
 sudo apt-get update
 # liblzma-dev is required by cohere package
 sudo apt-get install --no-install-recommends --yes \
-    libedit-dev \
-    zlib1g \
-    zlib1g-dev \
-    libssl-dev \
-    libbz2-dev \
-    libsqlite3-dev \
-    liblzma-dev
+  libedit-dev \
+  zlib1g \
+  zlib1g-dev \
+  libssl-dev \
+  libbz2-dev \
+  libsqlite3-dev \
+  liblzma-dev
 
-export PATH="$HOME/.asdf/shims:$PATH"
-asdf-global-installer.sh python
+script_dir=$(dirname "$0")
+cp -- $script_dir/python.toml ~/.config/mise/conf.d/
+# cp -- $script_dir/python ~/.config/mise/tasks/postinstall/
+mise install
 
-for package in pyright pipenv poetry ruff; do pipx install $package; done
-
-poetry completions fish > ~/.config/fish/completions/poetry.fish
+packages=(
+  pip
+  pyright
+)
+for package in "${packages[@]}"; do
+  pipx install $package
+done
