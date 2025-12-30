aed8b93171848c9336b0d27dfd6668b5138d82c3
Toraja <Toraja@users.noreply.github.com>
2025-12-30 18:03:14 +0900

    catalog/lua: unpin the lua version and remove luajit as it requries lua 5.1

--- catalog/lang/lua/install.sh
+++ catalog/lang/lua/install.sh
@@ -2,18 +2,14 @@
 set -eo pipefail
 
 sudo apt-get update
-# unzip is required by mise install lua
+# See https://github.com/mise-plugins/mise-lua for the required dependencies.
+# It should work without `linux-headers-$(uname -r)` package.
+# `libreadline-dev` is required to enable readline for lua 5.4 and above, when it is compiled with readline enabled.
+# Setting ASDF_LUA_LINUX_READLINE=1 will enable readline when installing via asdf (a backend mise uses).
 sudo apt-get install --no-install-recommends --yes \
-  build-essential libreadline-dev unzip
+  build-essential libreadline-dev
 
 script_dir=$(dirname "$0")
 cp -- $script_dir/lua.toml ~/.config/mise/conf.d/
 cp -- $script_dir/lua ~/.config/mise/tasks/postinstall/
-mise install
-
-# luajit is avaialbe as mise tool but it fails to install, so build manually instead
-tmpdir=$(mktemp --directory)
-git clone --depth 1 https://github.com/LuaJIT/LuaJIT.git $tmpdir
-cd $tmpdir
-make && sudo make install
-rm -rf $tmpdir
+ASDF_LUA_LINUX_READLINE=1 mise install
