5491a2ccbfb55b8b04ca638a08f0916c84f9b0ec
Toraja <Toraja@users.noreply.github.com>
2026-01-21 18:12:11 +0900

    catalog/git/glab: set GLAB_EDITOR to nvim

--- catalog/git/glab/install.sh
+++ catalog/git/glab/install.sh
@@ -4,3 +4,8 @@ set -eo pipefail
 script_dir=$(dirname "$0")
 cp -- $script_dir/glab.toml ~/.config/mise/conf.d/
 cp -- $script_dir/glab ~/.config/mise/tasks/postinstall/
+
+cat << EOF > ~/.config/fish/conf.d/my-glab.fish
+# glab CLI (as of v1.80.4) cannot handle multi-words value
+set --export GLAB_EDITOR nvim
+EOF
