e31d1bbeee042638c43368363ba61943173057a6
Toraja <Toraja@users.noreply.github.com>
2026-02-02 15:46:24 +0900

    fixup! catalog/llm: new

--- catalog/llm/install.sh
+++ catalog/llm/install.sh
@@ -3,12 +3,17 @@ set -eo pipefail
 
 script_dir=$(dirname "$0")
 catalog_name=$(basename "$script_dir")
+mise_install=false
 
 if [ -f "$script_dir/${catalog_name}.toml" ]; then
   cp -- "$script_dir/${catalog_name}.toml" ~/.config/mise/conf.d/
+  mise_install=true
 fi
 if ls "$script_dir/postinstall/"* > /dev/null 2>&1; then
   cp -- "$script_dir/postinstall/"* ~/.config/mise/tasks/postinstall/
+fi
+if $mise_install; then
+  mise install
 fi
 
 # Install specify-cli with `mise exec` since `uv` is supposed to be installed in python catalog
