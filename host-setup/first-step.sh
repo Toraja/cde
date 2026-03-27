5a71a56ea6d7256e7f2b437b41ce1f31da507dd7
Toraja <Toraja@users.noreply.github.com>
2026-03-27 16:21:26 +0900

    host-setup: move ansible installation to justfile

--- host-setup/first-step.sh
+++ host-setup/first-step.sh
@@ -5,12 +5,6 @@ set -euo pipefail
 script_dir=$(dirname "$0")
 fixture_dir=$script_dir/fixtures
 
-sudo apt-get update
-sudo apt-get install --no-install-recommends --yes pipx
-# Install ansible directly with pipx rather than with mise as I don't need to switch versions
-# and want to minimise disk usage
-pipx install --include-deps ansible
-
 # Create shell directories
 mkdir --parents ~/.bashrc.d ~/.local/share/bash-completion/completions/ ~/.config/fish/{completions,conf.d}
 
