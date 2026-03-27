440441dcd6c0afcb419c7c80b6bcf5420ffead0c
Toraja <Toraja@users.noreply.github.com>
2026-03-27 16:27:01 +0900

    host-setup: fix installation of bash completion

--- host-setup/first-step.sh
+++ host-setup/first-step.sh
@@ -8,14 +8,14 @@ fixture_dir=$script_dir/fixtures
 # Create shell directories
 mkdir --parents ~/.bashrc.d ~/.local/share/bash-completion/completions/ ~/.config/fish/{completions,conf.d}
 
+# Setup bashrc
+grep --quiet '# Added by host setup' ~/.bashrc || cat $fixture_dir/bashrc >> ~/.bashrc
+
 # Install mise
 PATH="$HOME/.local/bin:$PATH" # mise is installed under ~/.local/bin
 curl https://mise.run | sh
 cp --recursive $fixture_dir/mise ~/.config/
-mise completion bash > ~/.local/share/bash-completion/completions/mise.bash
+mise completion --include-bash-completion-lib bash > ~/.local/share/bash-completion/completions/mise.bash
 mise activate bash > ~/.bashrc.d/mise.bash
 mise completion fish > ~/.config/fish/completions/mise.fish
 mise install
-
-# Setup bashrc
-grep --quiet '# Added by host setup' ~/.bashrc || cat $fixture_dir/bashrc >> ~/.bashrc
