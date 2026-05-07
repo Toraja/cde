0098f058b1539a4ccd4c21ed6a36faf4909e877a
Toraja <Toraja@users.noreply.github.com>
2026-05-07 16:54:08 +0900

    root: add helper comment to proxy.sh

--- root/ctx/helpers/proxy.sh
+++ root/ctx/helpers/proxy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# -----------------------------------------------------------
+# Prerequisite: proxy must be set up in ~/.docker/config.json
+# -----------------------------------------------------------
+
 vars=(http_proxy https_proxy no_proxy)
 for var in ${vars[@]}; do
   val=$(eval echo \$$var)
