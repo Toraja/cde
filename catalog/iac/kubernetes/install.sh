dd38009dc3eb6749eb35eb54b01151b3c19d650e
Toraja <Toraja@users.noreply.github.com>
2026-05-13 14:57:52 +0900

    catalog: install helm-ls with mise

--- catalog/iac/kubernetes/install.sh
+++ catalog/iac/kubernetes/install.sh
@@ -11,5 +11,3 @@ if ls "$script_dir/postinstall/"* > /dev/null 2>&1; then
   cp -- "$script_dir/postinstall/"* ~/.config/mise/tasks/postinstall/
 fi
 mise install
-
-github-latest-release-installer.sh -x mrjosh helm-ls helm_ls_linux_amd64 ~/.local/bin/helm_ls
