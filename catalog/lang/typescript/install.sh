3e406379a20c44542eb8a89aebb54fc5d2425854
Toraja <Toraja@users.noreply.github.com>
2026-05-28 17:53:06 +0900

    catalog: add typescript

---
+++ catalog/lang/typescript/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eo pipefail
+
+script_dir=$(dirname "$0")
+catalog_name=$(basename "$script_dir")
+
+if [ -f "$script_dir/${catalog_name}.toml" ]; then
+  cp -- "$script_dir/${catalog_name}.toml" ~/.config/mise/conf.d/
+fi
+if ls "$script_dir/postinstall/"* > /dev/null 2>&1; then
+  cp -- "$script_dir/postinstall/"* ~/.config/mise/tasks/postinstall/
+fi
+# mise install
+
+mise exec node --command "npm install --global typescript typescript-language-server"
