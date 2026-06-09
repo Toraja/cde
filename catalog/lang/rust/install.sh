19ea7315f9b3fd7f31a85e8bc55b029f6a59e547
Toraja <Toraja@users.noreply.github.com>
2026-06-09 09:33:06 +0900

    catalog: change install location of codelldb (vscode extension)

--- catalog/lang/rust/install.sh
+++ catalog/lang/rust/install.sh
@@ -26,18 +26,12 @@ mise install
 mise exec rust cargo-binstall --command 'cargo binstall cargo-nextest --secure'
 
 # Used by rustaceanvim
-code_server_extension_dir=~/.local/share/code-server/extensions
-mkdir --parents $code_server_extension_dir
-codelldb_path=$(find $code_server_extension_dir -mindepth 1 -maxdepth 1 -type d -name "vadimcn.vscode-lldb*" | head -1)
-if [ -z "$codelldb_path" ]; then
-  workdir=/tmp/codelldb
-  mkdir --parents $workdir
-  github-latest-release-installer.sh vadimcn codelldb 'codelldb-linux-x64.vsix' $workdir/codelldb.vsix
-  unzip -o $workdir/codelldb.vsix -d $workdir
-  mv $workdir/extension.vsixmanifest $workdir/extension/.vsixmanifest
-  # Usually the directory name is vadimcn.vscode-lldb-<version>, but it is hard to determine the version, so just use vadimcn.vscode-lldb
-  mv $workdir/extension $code_server_extension_dir/vadimcn.vscode-lldb
-  rm --recursive --dir --force $workdir
-else
-  ln --symbolic $codelldb_path $code_server_extension_dir/vadimcn.vscode-lldb
-fi
+nvim_vscode_extension_dir=~/.local/share/nvim/vscode-extensions
+workdir=/tmp/codelldb
+mkdir --parents $nvim_vscode_extension_dir $workdir
+github-latest-release-installer.sh vadimcn codelldb 'codelldb-linux-x64.vsix' $workdir/codelldb.vsix
+unzip -o $workdir/codelldb.vsix -d $workdir
+mv $workdir/extension.vsixmanifest $workdir/extension/.vsixmanifest
+# Usually the directory name is vadimcn.vscode-lldb-<version>, but it is hard to determine the version, so just use vadimcn.vscode-lldb
+mv $workdir/extension $nvim_vscode_extension_dir/vadimcn.vscode-lldb
+rm --recursive --dir --force $workdir
