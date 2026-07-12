de8576e7840cbf8ff99a8c77f9dc3059cba0ee8d
Toraja <Toraja@users.noreply.github.com>
2026-07-13 08:45:46 +0900

    catalog: neovim setup does not have to be post-install

--- catalog/editor/neovim/install.sh
+++ catalog/editor/neovim/install.sh
@@ -17,3 +17,8 @@ if $mise_install; then
 fi
 
 # --- Add other commands ---
+
+mkdir --parents ~/.local/{share,state}/nvim/ ~/.cde/mnt/single/.local/state/nvim/shada/ ~/.cde/mnt/shared/.local/{share,state}/nvim/lazy/
+ln --symbolic --force ~/.cde/mnt/single/.local/state/nvim/{shada,trust} ~/.local/state/nvim/
+ln --symbolic --force ~/.cde/mnt/shared/.local/share/nvim/lazy ~/.local/share/nvim/
+ln --symbolic --force ~/.cde/mnt/shared/.local/state/nvim/lazy ~/.local/state/nvim/
--- catalog/editor/neovim/postinstall/neovim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-# --- Add post installation steps ---
-
-mkdir --parents ~/.local/{share,state}/nvim/ ~/.cde/mnt/single/.local/state/nvim/shada/ ~/.cde/mnt/shared/.local/{share,state}/nvim/lazy/
-ln --symbolic --force ~/.cde/mnt/single/.local/state/nvim/{shada,trust} ~/.local/state/nvim/
-ln --symbolic --force ~/.cde/mnt/shared/.local/share/nvim/lazy ~/.local/share/nvim/
-ln --symbolic --force ~/.cde/mnt/shared/.local/state/nvim/lazy ~/.local/state/nvim/
