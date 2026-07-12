f8a037ec5f5619a2f50b07ff08d824f48d259d2f
Toraja <Toraja@users.noreply.github.com>
2026-07-13 08:44:32 +0900

    catalog: fix opencode postinstall script

--- catalog/llm/postinstall/opencode.sh
+++ catalog/llm/postinstall/opencode.sh
@@ -2,5 +2,6 @@
 set -eo pipefail
 
 mkdir --parents ~/.config/opencode/
+# opencode config might need to be localised (custom providers, mcps) while tui.jsonc does not
 cp --force ~/toybox/opencode/opencode.jsonc ~/.config/opencode/
-cp --force ~/toybox/opencode/tui.jsonc ~/.config/opencode/
+ln --symbolic ~/toybox/opencode/tui.jsonc ~/.config/opencode/
