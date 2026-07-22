8a23f261f6a320fd33b1e00dca93c47b351375bf
Toraja <Toraja@users.noreply.github.com>
2026-07-22 14:16:48 +0900

    catalog: add packages necessary for pyenv

--- catalog/lang/python/install.sh
+++ catalog/lang/python/install.sh
@@ -3,8 +3,10 @@ set -eo pipefail
 
 sudo apt-get update
 # python3-venv is required by pipenv
+# The other packages are required to build Python from source, which is needed for pyenv (wihch is used by pipenv)
 sudo apt-get install --no-install-recommends --yes \
-  python3-venv
+  python3-venv \
+  make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libzstd-dev
 
 script_dir=$(dirname "$0")
 catalog_name=$(basename "$script_dir")
